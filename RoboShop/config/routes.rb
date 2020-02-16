5641080a8082bb0db2c0bf200e090178ca687dac
KacperDrewniak <50110237+KacperDrewniak@users.noreply.github.com>
2020-02-16 15:24:12 +0100

    install gems

--- RoboShop/config/routes.rb
+++ RoboShop/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static/terms'
+  get 'static/privacy'
+  get 'static/shipping'
+  get 'static/about'
+  
+
+
+  root to: 'products#index'
+  resources :products, only: [:show, :index]
+  resources :categories, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
